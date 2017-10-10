5c05c1f22e17a8dc380a3683b261b63f82823504
keulinho <jonas.elfering@web.de>
2017-10-10 14:52:19 +0200

    CON-4609: added setter to convert legacy product struct

--- src/main/Shopware/Connect/Struct/Change/ToShop/InsertOrUpdate.php
+++ src/main/Shopware/Connect/Struct/Change/ToShop/InsertOrUpdate.php
@@ -44,4 +44,21 @@ class InsertOrUpdate extends Change
 
     /** @var  string|null */
     public $groupId = null;
+
+    public function __set($name, $value)
+    {
+        //in legacy updater code it is called with ShopProduct struct -> this converts it to the new Product Struct
+        if ($name === 'product' && (!$value instanceof Product)) {
+            $this->product = new Product();
+            foreach ($value->toArray() as $name => $value) {
+                try {
+                    $this->product->$name = $value;
+                } catch (\Exception $e) {
+                    //catch everything try to convert legacy ShopProduct to new Product struct
+                }
+            }
+        } else {
+            parent::__set($name, $value);
+        }
+    }
 }
