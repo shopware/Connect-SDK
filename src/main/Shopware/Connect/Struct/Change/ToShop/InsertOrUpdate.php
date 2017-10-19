e5e47c87fb19d9a3855b93ccfa472991988eabd5
keulinho <jonas.elfering@web.de>
2017-10-19 10:20:47 +0200

    CON-4609: removed magic method to convert legacy structs

--- src/main/Shopware/Connect/Struct/Change/ToShop/InsertOrUpdate.php
+++ src/main/Shopware/Connect/Struct/Change/ToShop/InsertOrUpdate.php
@@ -23,21 +23,4 @@ class InsertOrUpdate extends ToShopChange
      * @var Product
      */
     public $product;
-
-    public function __set($name, $value)
-    {
-        //in legacy updater code it is called with ShopProduct struct -> this converts it to the new Product Struct
-        if ($name === 'product' && (!$value instanceof Product)) {
-            $this->product = new Product();
-            foreach ($value->toArray() as $name => $value) {
-                try {
-                    $this->product->$name = $value;
-                } catch (\Exception $e) {
-                    //catch everything try to convert legacy ShopProduct to new Product struct
-                }
-            }
-        } else {
-            parent::__set($name, $value);
-        }
-    }
 }
